971828ad30d178b734ab48e7df402b1c79db60a9
Clemens Rau <48509979+ClemensRau1337@users.noreply.github.com>
2021-02-14 12:40:50 +0100

    Create jtxtck.rb

---
+++ jtxtck.rb
@@ -0,0 +1,11 @@
+class jtxtck < Formula
+  desc "usefull script for TXT Record Check"
+  homepage "https://github.com/ClemensRau1337/jtxtck"
+
+
+  url "https://github.com/ClemensRau1337/jtxtck/get/HEAD.zip", :using => :curl
+
+  def install
+    bin.install "ClemensRau1337/jtxtck"
+  end
+end
